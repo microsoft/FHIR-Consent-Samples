e835d7c1b4d1f266b863c644c54cfe31ead3165e
Daniel Kim <dakim@microsoft.com>
2020-11-09 17:33:39 -0500

    added api code

---
+++ api/Controllers/ConsentController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using consent_api.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace consent_api.Controllers
+{
+    [Route("api/consents")]
+    [ApiController]
+    public class ConsentController : ControllerBase
+    {
+        // GET: api/consent
+        [HttpGet]
+        public string GetConsents([FromQuery(Name = "id")] string tempId)
+        {
+            string jsonResult;
+
+            if (tempId != null)
+            {
+                jsonResult = JsonConvert.SerializeObject(new
+                {
+                    results = new ConsentResult() { tempId = tempId, msg = "HELLO WORLD!" }
+                });
+            }
+            else
+            {
+                jsonResult = JsonConvert.SerializeObject(new
+                {
+                    results = new List<ConsentResult>()
+                    {
+                        new ConsentResult() { id = Guid.NewGuid(), upn = Guid.NewGuid(), msg = "Consent 1!" },
+                        new ConsentResult() { id = Guid.NewGuid(), upn = Guid.NewGuid(), msg = "Consent 2!" }
+                    }
+                });
+            }
+
+            return jsonResult;
+        }
+
+        // POST api/<ConsentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ConsentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ConsentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ api/Models/ConsentResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consent_api.Models
+{
+    public class ConsentResult
+    {
+        public Guid upn { get; set; }
+        public Guid id { get; set; }
+        public string msg { get; set; }
+        public string tempId { get; set; }
+    }
+}

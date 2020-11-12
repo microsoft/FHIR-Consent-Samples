ccda9facb1d9523606623ec35afb8a48fc49eed7
Daniel Kim <dakim@microsoft.com>
2020-11-12 10:27:25 -0500

    added mock consent endpoint

---
+++ api/ConsentsStore.cs
@@ -0,0 +1,24 @@
+﻿using consent_api.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consent_api
+{
+    public class ConsentsStore
+    {
+        public IEnumerable<Consent> getConsents()
+        {
+            return new List<Consent>()
+                    {
+                        new Consent() { id = "1", upn = "u1", status = true, msg = "Consent 1!" },
+                        new Consent() { id = "2", upn = "u2", status = true, msg = "Consent 2!" },
+                        new Consent() { id = "3", upn = "u1", status = true, msg = "Consent 3!" },
+                        new Consent() { id = "4", upn = "u2", status = false, msg = "Consent 4!" }
+                    };
+        }
+    }
+}

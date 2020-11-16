935ce58d1c6a64f65202af99916491a7638d627c
Guru Bandiatmakur <gubandia@microsoft.com>
2020-11-16 14:44:17 -0700

    Added unit test project.

---
+++ consent-api-unittest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace consent_api_unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

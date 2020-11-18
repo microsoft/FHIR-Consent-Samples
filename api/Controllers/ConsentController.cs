fafd8c4ae31d0d6b84be93088c2578b6da72941f
Daniel Kim <dakim@microsoft.com>
2020-11-18 15:20:46 -0500

    cherried 7d2349eea938acbc505476b4dc3147a13790a592

--- api/Controllers/ConsentController.cs
+++ api/Controllers/ConsentController.cs
@@ -22,17 +22,27 @@ namespace consent_api.Controllers
             return fhirconsents.ToString(); ;
         }
 
+<<<<<<< HEAD
+=======
+        // GET: api/consent
+        [HttpPut]
+        public void UpdateConsents([FromQuery] string id, [FromQuery] string upn, [FromQuery] string isActive)
+        {
+            fs.UpdateConsent(id, "0", isActive);
+        }
+
+>>>>>>> 7d2349e... updated the put function but it's not changing the value
         // POST api/<ConsentController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<ConsentController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/<ConsentController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE api/<ConsentController>/5
         [HttpDelete("{id}")]
